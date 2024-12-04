642ed2fcdc4a7b1074104bf99d815e85d3f691c3
Yannick Strelow <yannick.strelow@sn-invent.de>
2024-12-04 15:26:14 +0100

    Update for day 04

--- Day_04/Day04.kt
+++ Day_04/Day04.kt
@@ -1,89 +1,50 @@
 import java.io.File
+import kotlin.math.sign
 
 fun main() {
 
-    val xCoordinates = HashSet<Pair<Int, Int>>()
-    val mCoordinates = HashSet<Pair<Int, Int>>()
-    val aCoordinates = HashSet<Pair<Int, Int>>()
-    val sCoordinates = HashSet<Pair<Int, Int>>()
+    val charCoordinates = HashMap<Char, HashSet<Pair<Int, Int>>>()
 
     // read the coordinates of the relevant letters
     File("Day_04/input.txt").readLines().forEachIndexed { row, line ->
         line.toCharArray().forEachIndexed { index, ch ->
-            when (ch) {
-                'X' -> xCoordinates.add(row to index)
-                'M' -> mCoordinates.add(row to index)
-                'A' -> aCoordinates.add(row to index)
-                'S' -> sCoordinates.add(row to index)
-            }
+            if (charCoordinates[ch] == null) charCoordinates[ch] = HashSet()
+            charCoordinates[ch]?.add(row to index)
         }
     }
 
     // Part 1
     // create possible XMAS positions and check if the letter coordinate matches
-    xCoordinates.sumOf { (row, column) ->
+    charCoordinates['X']?.sumOf { (xRow, xColumn) ->
 
-        val possibleMPositions = setOf(
-            row - 1 to column - 1,
-            row - 1 to column,
-            row - 1 to column + 1,
-            row to column - 1,
-            row to column + 1,
-            row + 1 to column - 1,
-            row + 1 to column,
-            row + 1 to column + 1
-        )
-
-        val mPositions = possibleMPositions.filter { it in mCoordinates }
-        if (mPositions.isEmpty()) 0
-
-        val possibleAPositions = HashSet<Pair<Int, Int>>()
-        if (mPositions.contains(row - 1 to column - 1)) possibleAPositions.add(row - 2 to column - 2)
-        if (mPositions.contains(row - 1 to column)) possibleAPositions.add(row - 2 to column)
-        if (mPositions.contains(row - 1 to column + 1)) possibleAPositions.add(row - 2 to column + 2)
-        if (mPositions.contains(row to column - 1)) possibleAPositions.add(row to column - 2)
-        if (mPositions.contains(row to column + 1)) possibleAPositions.add(row to column + 2)
-        if (mPositions.contains(row + 1 to column - 1)) possibleAPositions.add(row + 2 to column - 2)
-        if (mPositions.contains(row + 1 to column)) possibleAPositions.add(row + 2 to column)
-        if (mPositions.contains(row + 1 to column + 1)) possibleAPositions.add(row + 2 to column + 2)
-
-        val aPositions = possibleAPositions.filter { it in aCoordinates }
-        if (aPositions.isEmpty()) 0
-
-        val possibleSPositions = HashSet<Pair<Int, Int>>()
-        if (aPositions.contains(row - 2 to column - 2)) possibleSPositions.add(row - 3 to column - 3)
-        if (aPositions.contains(row - 2 to column)) possibleSPositions.add(row - 3 to column)
-        if (aPositions.contains(row - 2 to column + 2)) possibleSPositions.add(row - 3 to column + 3)
-        if (aPositions.contains(row to column - 2)) possibleSPositions.add(row to column - 3)
-        if (aPositions.contains(row to column + 2)) possibleSPositions.add(row to column + 3)
-        if (aPositions.contains(row + 2 to column - 2)) possibleSPositions.add(row + 3 to column - 3)
-        if (aPositions.contains(row + 2 to column)) possibleSPositions.add(row + 3 to column)
-        if (aPositions.contains(row + 2 to column + 2)) possibleSPositions.add(row + 3 to column + 3)
-
-        possibleSPositions.count { it in sCoordinates }
+        arrayOf(
+            xRow - 1 to xColumn - 1,
+            xRow - 1 to xColumn,
+            xRow - 1 to xColumn + 1,
+            xRow to xColumn - 1,
+            xRow to xColumn + 1,
+            xRow + 1 to xColumn - 1,
+            xRow + 1 to xColumn,
+            xRow + 1 to xColumn + 1
+        ).filter { it in charCoordinates.getOrDefault('M', HashSet()) }
+            .map { (mRow, mColumn) -> xRow + ((mRow - xRow).sign * 2) to xColumn + ((mColumn - xColumn).sign * 2) }
+            .filter { it in charCoordinates.getOrDefault('A', HashSet()) }
+            .map { (aRow, aColumn) -> xRow + ((aRow - xRow).sign * 3) to xColumn + ((aColumn - xColumn).sign * 3) }
+            .count { it in charCoordinates.getOrDefault('S', HashSet()) }
 
     }.also { println("The number of XMAS words is $it ") }
 
     // Part 2
-    aCoordinates.sumOf { (row, column) ->
+    charCoordinates['A']?.sumOf { (aRow, aColumn) ->
 
-        val possibleMPositions = setOf(
-            row - 1 to column - 1,
-            row - 1 to column + 1,
-            row + 1 to column - 1,
-            row + 1 to column + 1
-        )
-
-        val mPositions = possibleMPositions.filter { it in mCoordinates }
-        if (mPositions.isEmpty()) 0
-
-        val possibleSPositions = HashSet<Pair<Int, Int>>()
-        if (mPositions.contains(row - 1 to column - 1)) possibleSPositions.add(row + 1 to column + 1)
-        if (mPositions.contains(row - 1 to column + 1)) possibleSPositions.add(row + 1 to column - 1)
-        if (mPositions.contains(row + 1 to column + 1)) possibleSPositions.add(row - 1 to column - 1)
-        if (mPositions.contains(row + 1 to column - 1)) possibleSPositions.add(row - 1 to column + 1)
-
-        possibleSPositions.count { it in sCoordinates } / 2
+        arrayOf(
+            aRow - 1 to aColumn - 1,
+            aRow - 1 to aColumn + 1,
+            aRow + 1 to aColumn - 1,
+            aRow + 1 to aColumn + 1
+        ).filter { charCoordinates['M']?.contains(it) == true }
+            .map { (mRow, mColumn) -> aRow + ((mRow - aRow) * -1) to aColumn + ((mColumn - aColumn) * -1) }
+            .count { charCoordinates['S']?.contains(it) == true } / 2
 
     }.also { println("The number of crossed MAS words is $it") }
 
