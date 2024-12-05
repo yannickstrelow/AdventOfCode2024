05bfe8fd9a80877ed7d0800ea04f1df5bfa040da
Yannick Strelow <yannick.strelow@sn-invent.de>
2024-12-05 15:53:37 +0100

    Update for day 04

--- Day_04/Day04.kt
+++ Day_04/Day04.kt
@@ -3,20 +3,21 @@ import kotlin.math.sign
 
 fun main() {
 
-    val charCoordinates = HashMap<Char, HashSet<Pair<Int, Int>>>()
+    val charCoordinates = mapOf(
+        'X' to HashSet<Pair<Int, Int>>(),
+        'M' to HashSet<Pair<Int, Int>>(),
+        'A' to HashSet<Pair<Int, Int>>(),
+        'S' to HashSet<Pair<Int, Int>>()
+    )
 
     // read the coordinates of the relevant letters
     File("Day_04/input.txt").readLines().forEachIndexed { row, line ->
-        line.toCharArray().forEachIndexed { column, ch ->
-            if (charCoordinates[ch] == null) charCoordinates[ch] = HashSet()
-            charCoordinates[ch]?.add(row to column)
-        }
+        line.toCharArray().forEachIndexed { column, ch -> charCoordinates[ch]?.add(row to column) }
     }
 
     // Part 1
     // create possible XMAS positions and check if the letter coordinate matches
     charCoordinates['X']?.sumOf { (xRow, xColumn) ->
-
         arrayOf(
             xRow - 1 to xColumn - 1,
             xRow - 1 to xColumn,
@@ -31,7 +32,6 @@ fun main() {
             .filter { it in charCoordinates.getOrDefault('A', HashSet()) }
             .map { (aRow, aColumn) -> xRow + ((aRow - xRow).sign * 3) to xColumn + ((aColumn - xColumn).sign * 3) }
             .count { it in charCoordinates.getOrDefault('S', HashSet()) }
-
     }.also { println("The number of XMAS words is $it ") }
 
     // Part 2
@@ -42,9 +42,9 @@ fun main() {
             aRow - 1 to aColumn + 1,
             aRow + 1 to aColumn - 1,
             aRow + 1 to aColumn + 1
-        ).filter { charCoordinates['M']?.contains(it) == true }
+        ).filter { it in charCoordinates.getOrDefault('M', HashSet()) }
             .map { (mRow, mColumn) -> aRow + ((mRow - aRow) * -1) to aColumn + ((mColumn - aColumn) * -1) }
-            .count { charCoordinates['S']?.contains(it) == true } / 2
+            .count { it in charCoordinates.getOrDefault('S', HashSet()) } / 2
 
     }.also { println("The number of crossed MAS words is $it") }
 
